7400f0d42665244687dee87e2fa14b4c8d74118f
behzad Arabi <behzadab15@gmail.com>
2020-06-02 15:25:16 +0430

    test paho

--- app/src/main/java/com/sanardev/instagrammqtt/ui/main/MainActivity.kt
+++ app/src/main/java/com/sanardev/instagrammqtt/ui/main/MainActivity.kt
@@ -10,11 +10,16 @@ import com.sanardev.instagrammqtt.R
 import com.sanardev.instagrammqtt.base.BaseActivity
 import com.sanardev.instagrammqtt.databinding.ActivityMainBinding
 import org.eclipse.paho.android.service.MqttService
-import org.fusesource.hawtbuf.Buffer
-import org.fusesource.hawtbuf.UTF8Buffer
-import org.fusesource.mqtt.client.Callback
-import org.fusesource.mqtt.client.Listener
-import org.fusesource.mqtt.client.MQTT
+import org.eclipse.paho.client.mqttv3.MqttClient
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions
+import org.eclipse.paho.client.mqttv3.MqttException
+import org.eclipse.paho.client.mqttv3.MqttMessage
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() ,Connection.IReceivedMessageListener{
@@ -47,11 +52,67 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() ,Connect
         super.onPause()
     }
 
-    private val DEFAULT_HOST = "mqtt-mini.facebook.com"
+    private val DEFAULT_HOST = "ssl://mqtt-mini.facebook.com:443"
     private val DEFAULT_PORT = 443
     @Throws(Exception::class)
     fun connect(protogle: String) {
-        ContextCompat.startForegroundService(this@MainActivity,Intent(this@MainActivity, MqttService::class.java))
+        val topic = "MQTT Examples"
+        val content = "Message from MqttPublishSample"
+        val qos = 2
+        val broker = DEFAULT_HOST
+        val clientId = "882bf190-b878-448e-4"
+        val persistence = MemoryPersistence()
+
+        try {
+            val trustAllCerts: Array<TrustManager> = arrayOf<TrustManager>(
+                object : X509TrustManager {
+                    override fun checkClientTrusted(
+                        chain: Array<out X509Certificate>?,
+                        authType: String?
+                    ) {
+
+                    }
+
+                    override fun checkServerTrusted(
+                        chain: Array<out X509Certificate>?,
+                        authType: String?
+                    ) {
+                    }
+
+                    override fun getAcceptedIssuers(): Array<X509Certificate>? {
+                        return null
+                    }
+                }
+            )
+            val sslContext: SSLContext = SSLContext.getInstance("TLS")
+            sslContext.init(null, trustAllCerts, SecureRandom())
+
+            val sampleClient = MqttClient(broker, clientId, persistence)
+            val connOpts = MqttConnectOptions()
+            connOpts.socketFactory = sslContext.socketFactory
+            connOpts.userName = null
+            connOpts.password = null
+            connOpts.keepAliveInterval = 900
+            connOpts.isCleanSession = true
+            Log.i("TEST_APPLICATION","Connecting to broker: $broker")
+            sampleClient.connect(connOpts)
+            Log.i("TEST_APPLICATION","Connected")
+            Log.i("TEST_APPLICATION","Publishing message: $content")
+            val message = MqttMessage(content.toByteArray())
+            message.qos = qos
+            sampleClient.publish(topic, message)
+            Log.i("TEST_APPLICATION","Message published")
+            sampleClient.disconnect()
+            Log.i("TEST_APPLICATION","Disconnected")
+            System.exit(0)
+        } catch (me: MqttException) {
+            Log.i("TEST_APPLICATION","reason " + me.reasonCode)
+            Log.i("TEST_APPLICATION","msg " + me.message)
+            Log.i("TEST_APPLICATION","loc " + me.localizedMessage)
+            Log.i("TEST_APPLICATION","cause " + me.cause)
+            Log.i("TEST_APPLICATION","excep $me")
+            me.printStackTrace()
+        }
     }
 
     companion object {
